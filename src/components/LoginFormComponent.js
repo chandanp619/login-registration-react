a9e38e2e97e9183cd0f1d7b967b926a206f69e0f
chandanp619 <chandanp619@gmail.com>
2024-01-14 08:46:18 +0530

    added bootstrap and 2 components.

---
+++ src/components/LoginFormComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginFormComponent(props) {
+    return (
+        <div>
+            Login
+        </div>
+    );
+}
+
+export default LoginFormComponent;
---
+++ src/components/RegistrationComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegistrationComponent(props) {
+    return (
+        <div>
+            Registration
+        </div>
+    );
+}
+
+export default RegistrationComponent;
