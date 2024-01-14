c80ee600692ef573518121a36484a6fab9d0a438
chandanp619 <chandanp619@gmail.com>
2024-01-14 16:03:13 +0530

    added forms

--- src/components/LoginFormComponent.js
+++ src/components/LoginFormComponent.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 function LoginFormComponent(props) {
+
+    const processLogin = () => {
+        
+    }
     return (
-        <div>
-            Login
+        <div className='container border rounded-3 mt-5 mb-5 p-3'>
+            <div className='row'>
+                <div className='col d-flex justify-content-start mb-5'>
+                    <h4>Login</h4>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='email' placeholder='Email Address' className='form-control' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='password' placeholder='Password' className='form-control' />
+                </div>
+            </div>
+                
+            <div className='row'>
+                <div className='col'>
+                    <button className='btn btn-primary' onClick={processLogin}>Login</button>
+                </div>
+
+            </div>       
         </div>
     );
 }
--- src/components/RegistrationComponent.js
+++ src/components/RegistrationComponent.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
 function RegistrationComponent(props) {
+
+    const processRegister = ()=>{
+        
+    };
     return (
-        <div>
-            Registration
+        <div className='container border rounded-3 mt-5 mb-5 p-3'>
+            <div className='row'>
+                <div className='col d-flex justify-content-start mb-5'>
+                    <h4>Register</h4>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='email' placeholder='Email Address' className='form-control' autoComplete='off' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='password' placeholder='Password' className='form-control' autoComplete='off' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='password' placeholder='Confirm Password' className='form-control' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='text' placeholder='First Name' className='form-control' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2'>
+                    <input type='text' placeholder='Last Name' className='form-control' />
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col mb-2 d-flex justify-content-start'>
+                        <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male" />
+                        <label class="form-check-label" for="inlineRadio1">Male</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="female" />
+                        <label class="form-check-label" for="inlineRadio2">Female</label>
+                        </div>
+                </div>
+            </div>     
+            <div className='row'>
+                <div className='col'>
+                    <button className='btn btn-primary' onClick={processRegister}>Register</button>
+                </div>
+
+            </div>       
         </div>
     );
 }
